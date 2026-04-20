bf4e7c415f8b9f0400c1d096e08b2e5f3bb0808e
Hannah Sofie <hannah-sofie@live.no>
2026-04-20 14:28:23 +0200

    fix: allow frontend origin with cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://countries-case-enhanced.vercel.app",
+    ],
+  })
+);
 
 app.get("/api/countries", async (req, res) => {
   try {
